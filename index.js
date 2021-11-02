4d8b20cf1d77d38964c17e3fd8d3e16d3aa38b0d
nluuarch@gmail.com <nluuarch@gmail.com>
2021-11-01 18:53:30 -0700

    commit tree 3d82b9097b6756fa0343d66398c59374d0b46862

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let num;
+
+function scuberGreetingForFeet(num) {
+  if (num <= 400){
+    return console.log('This one is on me!');
+  } else if (num > 2000) {
+    return console.log('I will gladly take your thirty bucks.');
+  } else if (num > 2500) {
+    return console.log('No can do.')
+  }
 }
 
+
+// ^^^ Works in repl?
+// AssertionError: expected undefined to equal 'This one is on me!'
+
+
 function ternaryCheckCity(){
-  // Write your code here!
+  
 }
+
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
